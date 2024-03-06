5efe11bec81b01d15b9076d09c1936939b0a1725
zendude <ericluttrell08@gmail.com>
2024-03-06 12:35:16 -0700

    changed pages

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import dash_bootstrap_components as dbc
 from datetime import date
 
 # Import the page layouts from the pages folder
-from pages import ir_cam_dashboard, nitrogen_tank_dashboard, southpole_rfi_dashboard
+from pages import southpole_rfi_dashboard
 
 # Initialize Dash app directly here
 app = Dash(__name__, suppress_callback_exceptions=True, external_stylesheets=[dbc.themes.BOOTSTRAP])
@@ -14,8 +14,6 @@ server = app.server  # Expose the Flask server for Gunicorn
 navbar = dbc.NavbarSimple(
     children=[
         dbc.NavItem(dbc.NavLink("Home", href="/")),
-        dbc.NavItem(dbc.NavLink("IR Cam Dashboard", href="/ir_cam_dashboard")),
-        dbc.NavItem(dbc.NavLink("Nitrogen Tank Dashboard", href="/nitrogen_tank_dashboard")),
         dbc.NavItem(dbc.NavLink("Southpole RFI Dashboard", href="/southpole_rfi_dashboard")),
     ],
     brand="Dr. Barron Cosmology Lab",
@@ -24,8 +22,7 @@ navbar = dbc.NavbarSimple(
     dark=True,
 )
 
-# Assuming page layouts are defined in the same file or imported correctly
-# For demonstration, define a simple layout for `home`
+# define layout for landing page 
 home_layout = html.Div([
     html.H1("Home Page"),
     html.P("Welcome to the Dash app!")
@@ -42,13 +39,7 @@ app.layout = html.Div([
 @app.callback(Output('page-content', 'children'),
               [Input('url', 'pathname')])
 def display_page(pathname):
-    if pathname == '/ir_cam_dashboard':
-        return ir_cam_dashboard.layout
-        pass
-    elif pathname == '/nitrogen_tank_dashboard':
-        return nitrogen_tank_dashboard.layout
-        pass
-    elif pathname == '/southpole_rfi_dashboard':
+    if pathname == '/southpole_rfi_dashboard':
         return southpole_rfi_dashboard.layout
         pass
     else:
